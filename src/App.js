0246bd4ffd40acfdfb2f043f35781826ab4d9391
이황제 <happy_kaiser@hotmail.com>
2023-10-30 01:02:17 +0900

    get, set setter 함수 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import MESSAGE from "./contants/message";
 import CONDITION from "./contants/condition";
 
 class App {
+  #cars = new Map();
+
+  #numberOfTimes = 0;
+
+  get cars() {
+    return this.#cars;
+  }
+
+  set cars(info) {
+    this.#cars = info;
+  }
+
+  get numberOfTimes() {
+    return this.#numberOfTimes;
+  }
+
+  set numberOfTimes(number) {
+    this.#numberOfTimes = number;
+  }
+
   async play() {}
 }
 
