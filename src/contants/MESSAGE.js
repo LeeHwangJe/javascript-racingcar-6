59635833bfb02f5997f7215b572818f42d1d61bd
이황제 <happy_kaiser@hotmail.com>
2023-10-30 16:22:07 +0900

    feat: 오류 메시지 추가

--- src/contants/MESSAGE.js
+++ src/contants/MESSAGE.js
@@ -3,6 +3,7 @@ export const ERROR_MESSAGE = {
   NOT_NUMBER: "[ERROR] 숫자를 입력해주세요.",
   SHORT_LENGTH: "[ERROR] 2개 이상의 이름을 입력해주세요.",
   LONG_LENGTH: "[ERROR] 5자리 이하의 이름을 입력해주세요.",
+  WRONG_TYPE: "[ERROR] 잘못된 값을 입력하셨습니다.",
 };
 
 export const TEXT = {
