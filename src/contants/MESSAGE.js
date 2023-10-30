0c59c4b960d9606abf56c1693be995015fc96a3f
이황제 <happy_kaiser@hotmail.com>
2023-10-30 15:27:19 +0900

    feat: 에러 메시지 추가

---
+++ src/contants/MESSAGE.js
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGE = {
+  DUPLICATE_ERROR: "[ERROR] 이름이 중복됩니다.",
+  NOT_NUMBER: "[ERROR] 숫자를 입력해주세요.",
+  SHORT_LENGTH: "[ERROR] 2개 이상의 이름을 입력해주세요.",
+  LONG_LENGTH: "[ERROR] 5자리 이하의 이름을 입력해주세요.",
+};
