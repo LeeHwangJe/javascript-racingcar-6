97e754f8e67182d4973440a0a707892e4a4e2b7c
이황제 <happy_kaiser@hotmail.com>
2023-10-30 15:29:01 +0900

    feat: 입력 메시지 추가

--- src/contants/MESSAGE.js
+++ src/contants/MESSAGE.js
@@ -4,3 +4,10 @@ export const ERROR_MESSAGE = {
   SHORT_LENGTH: "[ERROR] 2개 이상의 이름을 입력해주세요.",
   LONG_LENGTH: "[ERROR] 5자리 이하의 이름을 입력해주세요.",
 };
+
+export const TEXT = {
+  CAR_NAMES: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  TRY_NUMBER: "시도할 횟수는 몇 회인가요?\n",
+  RACING_RESULT: "최종 우승자 : ",
+  ENTER: "",
+};
