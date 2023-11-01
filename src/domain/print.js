3c85961e2814ee7518b2af4dc1acce3b94edb659
이황제 <happy_kaiser@hotmail.com>
2023-11-01 15:42:22 +0900

    feat:최종 결과 출력 과정

--- src/domain/print.js
+++ src/domain/print.js
@@ -1,2 +1,10 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { TEXT } from "../contants/MESSAGE";
+import { howManyFirstCars, makeWinnerArray } from "./controller";
+import(howManyFirstCars);
+
+export const racingProgressPrint = (carNamesArray, countArr, j) => {
+  MissionUtils.Console.print(
+    carNamesArray[j] + " : " + countArr[j].join("").trim()
+  );
+};
